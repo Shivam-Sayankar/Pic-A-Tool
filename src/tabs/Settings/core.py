2c8e4f40b49172233e03814d7347bb82489b4b32
Shivam Sayankar <dr.strange996@gmail.com>
2024-12-08 14:04:24 +0530

    moved appearance, color theme functions to core.py + added backup folder function

--- src/tabs/Settings/core.py
+++ src/tabs/Settings/core.py
@@ -1,4 +1,6 @@
+import customtkinter as ctk
 import json
+import os
 
 def load_settings():
     try:
@@ -26,3 +28,42 @@ def change_settings(key, value):
     with open("src/config/settings.json", "w") as file:
         settings_data["current"][key] = value
         json.dump(settings_data, file, indent=4)
+
+
+
+def change_appearance(appareance_mode, btns_frame, phone_images_tab):
+    selected_mode = appareance_mode.get()
+    print(f"Selected Appearance Mode: {selected_mode}")
+    ctk.set_appearance_mode(selected_mode)  # Change the theme dynamically
+    btns_frame.configure(fg_color=phone_images_tab.cget("fg_color")) 
+
+    change_settings("appearance", selected_mode)
+
+
+def change_color_theme(color_theme):
+    selected_mode = color_theme.get()
+    print(f"Selected Color Theme: {selected_mode}")
+    ctk.set_default_color_theme(selected_mode)  # Change the theme dynamically
+
+    change_settings("color_theme", selected_mode)
+
+
+def take_backup_switch(backup_switch_var, backup_folder_entry, browse_backup_folder_btn, select_backup_folder_btn):
+    
+    if backup_switch_var == "on":
+        change_settings("take_backup", "yes")
+        backup_folder_entry.configure(state="normal")
+        browse_backup_folder_btn.configure(state="normal")
+        select_backup_folder_btn.configure(state="normal")
+        
+    else:
+        change_settings("take_backup", "no")
+        backup_folder_entry.configure(state="disabled")
+        browse_backup_folder_btn.configure(state="disabled")
+        select_backup_folder_btn.configure(state="disabled")
+
+
+def select_backup_folder(folder_path):
+
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        change_settings("backup_folder", folder_path)
